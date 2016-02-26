7fda823c6acfd3e165190767c1f5594bed1a721a
engineer-pearl <theadavis101@gmail.com>
2016-02-25 20:21:13 -0800

    Feb 25 second update
    **MODIFICATIONS**
    
    Files changed to javascript
    Post commands combined into one file
    Group commands added to single file
    Some commands given better comment structure
    Multiple unnessasary files removed
    
    **KNOWN ISSUES**
    
    Commands still need to be brought into proper structure
    Outside Server is not being tracked by Git for the main repository
    
    **Miscelanious**
    
    Need to remove Gedit's randomly added files

---
+++ bin/main.cpp
@@ -0,0 +1,10 @@
+//Start starting up everything
+//include <client face> // seperate from this file, making it easy to replace or change
+//incllude <command list> //seperate in case of updates or for diffirent clients which have commands especially for interacting with compatible servers
+//include <error sheet> //that way I can start with the html stuff and add more as I go?
+//confirm user and yada yada
+//connect to stuff from the list
+//idk should probably take stuff and use it to send and write
+//how does a client work?
+
+//Okay you caught me. I don't remember how to c++
