b6ae1dcfa332f23568922b3757df3b8b30315e53
engineer-pearl <theadavis101@gmail.com>
2016-06-05 15:27:02 -0700

    Begining to encorperate networking protocal.
    Preparing to build to support the Tumblr API.
    
    Preparing to include Cmake
    
    Created a dependancies list

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//outside dependencies
+#include <string> //without using namespace std; I believe I start everything from here with std:
+#include "Poco/String.h" //the new thing in .cpp_additions
+//Okay so apparently there's a thing alled utf-8. I have no idea what I am doing here
+// but if it does wind up that I have it, I need to use poco::utf8String class... what the hell does that mean?
+
+//include <client face> // seperate from this file, making it easy to replace or change
+//incllude <command list> //seperate in case of updates or for diffirent clients which have commands especially for interacting with compatible servers
+//include <error sheet> //that way I can start with the html stuff and add more as I go?
+
+//start up everything
+//confirm user and yada yada
+//connect to stuff from the list
+//idk should probably take stuff and use it to send and write
+//how does a client work?
+
+//Okay you caught me. I don't remember how to c++
